878d0485933a8bf8f22ec967f686ab23f607d156
IdinGanteng <kanggayus101@gmail.com>
2026-01-28 22:55:37 +0700

    final

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ChatApp from "./components/ChatApp";
 export default function App() {
   return (
     <div>
-      <h2 style={{ textAlign: "center" }}>🤖 Chat AI</h2>
+      <h2 style={{ textAlign: "center" }}>🤖 LXD AI CUSTOMER SERVICE</h2>
       <ChatApp />
     </div>
   );
