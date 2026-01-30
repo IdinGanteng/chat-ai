50de10d42ab1dd323b7c2b8c2dda75b28a5293e8
IdinGanteng <kanggayus101@gmail.com>
2026-01-31 01:33:16 +0700

    ubah judul

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import ChatApp from "./components/ChatApp";
 export default function App() {
   return (
     <div>
-      <h2 style={{ textAlign: "center" }}>🤖 LXD AI CUSTOMER SERVICE</h2>
       <ChatApp />
     </div>
   );
